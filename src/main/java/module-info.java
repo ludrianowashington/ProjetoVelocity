1fdc50d3dc392013a4f1b90dc3fed6a2eafc176e
Ludriano Washington <ludinho.washi@gmail.com>
2024-12-20 12:06:05 -0300

    Revert "etapa inicial"
    This reverts commit 09793323e045017f42d9a2b09cc94506f3437688.

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.projetovelocity {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.projetovelocity to javafx.fxml;
-    exports com.example.projetovelocity;
-}
